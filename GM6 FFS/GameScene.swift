75d3428019edfaa04ca9335aa46f3bfa404aedf6
fluidityt <hackbook17@justins-macbook-pro.local>
2017-03-08 14:26:12 -0500

    cool

--- GM6 FFS/GameScene.swift
+++ GM6 FFS/GameScene.swift
@@ -1,89 +1,361 @@
+import SpriteKit
+
+//
+//  Enums.swift
+//
+
+enum Modes { case swap }
+
+enum sizes {
+  static  let
+  prompt = CGSize(width: 50, height: 25),
+  choice = CGSize(width: 200, height: 10)
+  
+  static func stretchedSize(numChildren: Int) -> CGSize {
+    return CGSize(width: prompt.width, height: choice.height * 5.6)
+  }
+}
+
+enum sys {
+  
+  static var
+  scene: SKScene = SKScene(),
+  igeCounter = 0,
+  currentNode: IGE?,                    // NP
+  frames: [String: CGRect] = [:],
+  collided: IGE?
+  
+  enum touches {
+    static var
+    tb: (IGE, CGPoint)?,
+    tm: (IGE, Any?)?,
+    te: IGE?
+    
+    static func noTouchErrors() -> Bool { // Can't have two touch commands at update.
+      // FIXME: Add error messages or change to guard... and check for more than 1.
+      if tb != nil {
+        if tm != nil      { return false }
+        else if te != nil { return false }
+      }
+      else if tm != nil {
+        if tb != nil      { return false }
+        else if te != nil { return false }
+      }
+      else if te != nil {
+        if tb != nil      { return false }
+        else if tm != nil { return false }
+      }
+      return true
+    }
+    
+    static func resetTouches() { tb = nil; tm = nil; te = nil }
+  }
+  
+  /// Use this at various times... when sorting / swapping / deleting / adding iges.
+  static func render(from ige: IGE) { // NP
+    // Basically just a bunch of algos that adjust iges position.
+    // So it isn"t overwhelming.. just render them at the correct Choice and then work on constraints later.
+  }
+  
+  static func swapChoices(detectedChoice: inout Choice) {
+    guard sys.currentNode is Choice else { fatalError("swapChoice: sys.curnode ! choice") }
+    print("swapping choices")
+    
+    let tempNode    = sys.currentNode!.copy() as! Choice // For use with detectedChoice.
+    sys.currentNode = detectedChoice.copy()   as! Choice
+    detectedChoice  = tempNode.copy()         as! Choice
+    
+    // FIXME: determine which one is further left
+    detectedChoice.align()
+    (sys.currentNode! as! Choice).align()
+    
+    // guard var curNode = sys.currentNode as? Choice else { fatalError("swapChoice: not a Choice selected") }
+    // swap(&curNode, &detectedChoice)
+  }
+};
+
+//
+// IGE init:
+//
+
+class IGE: SKSpriteNode {
+  
+  override var parent: SKNode? { fatalError(" dont call parent!") }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    sys.touches.tb = (self, touches.first!.location(in: scene!))
+  }
+  
+  private func findName(title: String) -> String {
+    let myType = String(describing: type(of: self))
+    return (myType + ": " + title + String(sys.igeCounter))
+  }
+  
+  private func findColor() -> SKColor {
+    let myType = String(describing: type(of: self))
+    if myType == "Prompt" || myType == "Super" {
+      return .blue
+    } else { return .red }
+  }
+  
+  private func findSize() -> CGSize {
+    let myType = String(describing: type(of: self))
+    if myType == "Prompt" || myType == "Super" {
+      return sizes.prompt
+    } else { return sizes.choice }
+  }
+  
+  init(title: String) {
+    super.init(texture: nil, color: .black, size: CGSize.zero)
+    sys.igeCounter += 1
+    
+    name        = findName(title: title)
+    color       = findColor()
+    size        = findSize()
+    anchorPoint = CGPoint.zero
+    isUserInteractionEnabled = true
+  }
+  
+  required init?(coder aDecoder: NSCoder) { fatalError("fe")}
+
+};
+
+// FIXME: Make a protocol
+class IGE_CanDraw: IGE {
+  
+  var childs: [Choice] = []
+  
+  func draw() {
+    
+    var y = CGFloat(0)
+    for child in childs {
+      child.position = position
+      child.position.y += y
+      child.position.x += frame.width + 10
+      child.align()
+      y += 30 // Set height for next one.
+    }
+    y = 0
+  }
+  
+  func resize() {
+    size = CGSize(width: size.width, height: CGFloat((childs.count ) * 30))
+    switch (childs.count) {
+    case 0:  print("resize: no childs found")
+    default: print("resize: no case found")
+    }
+    sys.frames[name!] = frame
+  }
+  
+  override func addChild(_ node: SKNode) {
+    guard let child = node as? Choice else { print("addChild: not a choice"); return }
+    
+    scene!.addChild(child)
+    childs.append(child)
+    sys.currentNode = child
+    
+    draw()
+    resize()
+  }
+}
+
+// Super:
+final class Super: IGE_CanDraw {
+}
+
+// Prompts:
+final class Prompt: IGE_CanDraw {
+  
+  var mother: Choice
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    position = touches.first!.location(in: scene!)
+    draw()
+  }
+  
+  override func addChild(_ node: SKNode) {
+    guard let child = node as? Choice else { print("addChild: not a choice"); return }
+    child.mother = self
+    
+    scene!.addChild(child)
+    childs.append(child)
+    sys.currentNode = child
+    
+    draw()
+    resize()
+  }
+  
+  init(title: String, mother: Choice) {
+    self.mother = mother
+    super.init(title: title)
+  }
+  required init?(coder aDecoder: NSCoder) { fatalError("") }
+};
+
+// Choice:
+final class Choice: IGE {
+  
+  var mother: IGE_CanDraw
+  weak var child: IGE?
+  
+  func align() {
+    guard let theChild = child else { print("align: no child"); return }
+    theChild.position = position
+    theChild.position.x += frame.width + 10
+    (theChild as! Prompt).draw()
+  }
+  
+  override func addChild(_ node: SKNode) {
+    if child != nil { print("addChild: already has prompt"); return }
+    guard let prompt = node as? Prompt else { print("not a prompt"); return }
+    prompt.mother = self
+    
+    scene!.addChild(prompt)
+    child = prompt
+    sys.currentNode = child
+    
+    align()
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    position = touches.first!.location(in: scene!)
+    align()
+  }
+  
+  init(title: String, mother: IGE_CanDraw) {
+    self.mother = mother
+    super.init(title: title)
+  }
+  required init?(coder aDecoder: NSCoder) { fatalError("") }
+  
+};
+
+//
+//  Buttons.swift
+//
+
+// Buttons:
+class Button: SKSpriteNode {
+  
+  init(color: SKColor, size: CGSize) {
+    super.init(texture: nil, color: color, size: size)
+    isUserInteractionEnabled = true
+  }
+  
+  required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented")  }
+  
+  func promptChoiceCode(theNode: SKNode, runIfPrompt: ()->(), runIfChoice: ()->() ) {
+    if theNode is Prompt {
+      runIfPrompt()
+    } else if theNode is Choice {
+      runIfChoice()
+    } else { fatalError("not a correct type") }
+  }
+};
+
+final class AddButton: Button {
+  // Need to figure out based on Y value where to put new prompt at... will need a line.
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    guard let curNode = sys.currentNode else { print("tb: no node selected"); return }
+    print("<<", curNode.name as Any, ">> is selected")
+    
+    if let      cn = curNode as? Prompt {
+      cn.addChild(Choice(title: "added prompt", mother: cn))
+      cn.draw()
+    }
+    else if let cn = curNode as? Super {
+      cn.addChild(Choice(title: "added super", mother: cn))
+      cn.draw()
+    }
+    else if let cn = curNode as? Choice {
+      cn.addChild(Prompt(title: "added choice", mother: cn))
+      // FIXME: improve this:
+      if sys.igeCounter > 1 { cn.color = SKColor.green }
+    }
+    
+  }
+};
+
 //
 //  GameScene.swift
-//  GM6 FFS
 //
-//  Created by justin fluidity on 3/8/17.
-//  Copyright © 2017 justin fluidity. All rights reserved.
-//
-
-import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
+  
+  private func initialize() {
+    // Laundry list:
+    sys.scene = self
+    let addButton = AddButton(color: .green, size: CGSize(width: 200, height: 200))
+    addButton.position.x = frame.minX
+    addButton.position.y -= 300
+    addChild(addButton)
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let bkg = SKSpriteNode(color: .gray, size: size)
+    bkg.isUserInteractionEnabled = true
+    bkg.zPosition -= 1
+    bkg.name = "bkg"
+    addChild(bkg)
+  }
+  
+  private func test() {
+    let zip = Super(title: "new prompt"); ZIPSTUFF: do {
+      addChild(zip)
+      zip.position.x = frame.minX
+      zip.resize()
+      sys.currentNode = zip
+    }
+  }
+  
+  override func didMove(to view: SKView) {
+    initialize()
+    test()
+  }
+  
+  private func doCollision(for child: IGE) {
+    // Should probably use multi-switch here...
+    // FIXME: Will need to determine which one is closest for multi-hits
+    if let prompt = child as? Prompt { // Determine if hit node is a prompt or choice:
+    } else if var choice = child as? Choice {
+      if sys.currentNode is Choice {
+        sys.swapChoices(detectedChoice: &choice)
+      }
+    }
+    sys.collided = nil // FIXME: not sure if this works
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if let collided = sys.collided { doCollision(for: collided) }
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
     
-    override func didMove(to view: SKView) {
+    
+    TOUCHES: do {
+      guard sys.touches.noTouchErrors() else { break TOUCHES }
+      if let ige = sys.touches.tb?.0 {
+        sys.currentNode = ige
+      }
+    }
+    
+    HITDETECT: do {
+      
+      for child in children {
+        if child.name == "bkg" { continue }
+        if child.name == sys.currentNode!.name { continue }
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        if sys.currentNode!.frame.intersects(child.frame) {
+          print("hit detected")
+          sys.collided = child as! IGE
+          return          // Early exit.
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+      }
+      sys.collided = nil  // FIXME: Make sure this works...
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
+};
+
+func stuff() {
+
+
 }
